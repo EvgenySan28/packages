2647213d96cfe2652e7a70603b358700c389e4b2
Евгений Ульянцев <belfegorrev@outlook.com>
2021-08-23 20:10:23 +0300

    Добавьте файлы проекта.

---
+++ TaskTests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = new Task<int>(() => { Console.WriteLine("34"); return 34; });
+            Task<int> t2 = new Task<int>(() => { Console.WriteLine("11"); return 11; });
+            t.Start();
+            t2.RunSynchronously();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
